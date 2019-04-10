b3d1e7424c466061b4940eb8afbf2c7dbd814189
Tassio Lima <tassio.pereira.lima@accenture.com>
2019-04-10 13:26:48 -0300

    create new feature file and steps location

--- features/step_definitions/search_pokemon_step.rb
+++ features/step_definitions/search_pokemon_step.rb
@@ -9,9 +9,13 @@
   end
   
   When("search a pokemon {string}") do |string|
-    @get_result = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{string}")
+    @get_result_string = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{string}")
   end
   
   Then("wild pokemon disappeared!") do
-    expect(@get_result.code).to eq 404
+    expect(@get_result_string.code).to eq 404
+  end
+
+  Then("all wild pokemon appeared!") do
+    expect(@get_result_string.code).to eq 200
   end
